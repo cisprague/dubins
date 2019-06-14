6592fa0d353f21592dcb27ab1b046517a30ab599
cisprague <christopher.iliffe.sprague@gmail.com>
2019-06-14 15:12:31 +0200

    Fixed time-step cheat

--- dubins.py
+++ dubins.py
@@ -16,7 +16,7 @@ def evaluate(solution_function, obs, verbose=False):
     controls, times = solution_function(car)
 
     # return state, control, and time lists, along with doneness and car
-    xl, yl, thetal, ul, tl, safe, done = car.evaluate(controls, times, verbose=verbose)
+    xl, yl, thetal, ul, tl, safe, done = car_evaluate(car, controls, times, verbose=verbose)
     return car, xl, yl, thetal, ul, tl, safe, done
 
 
@@ -42,9 +42,6 @@ class Car(object):
         self.xt = self._environment.lx
         self.yt = self._environment.ly*0.8
 
-        # time step size [s]
-        self.dt = 0.01
-
         # list of obstacles
         self.obs = [(ob.x, ob.y, ob.r) for ob in self._environment.obstacles]
 
@@ -54,97 +51,95 @@ class Car(object):
         self.ylb = float(0)
         self.yub = self._environment.ly
 
-    def step(self, x, y, theta, phi):
+def step(car, x, y, theta, phi, dt=0.01):
 
-        '''
-        Returns a new state (xn, yn, thetan), safety condition,
-        and done condition, given an initial state (x, y, theta)
-        and control phi.
-        Numerical integration is done at a time step of dt [sec].
-        '''
+    '''
+    Returns a new state (xn, yn, thetan), safety condition,
+    and done condition, given an initial state (x, y, theta)
+    and control phi.
+    Numerical integration is done at a time step of dt [sec].
+    '''
 
-        # state rate
-        dx     = cos(theta)
-        dy     = sin(theta)
-        dtheta = tan(phi)
+    # state rate
+    dx     = cos(theta)
+    dy     = sin(theta)
+    dtheta = tan(phi)
 
-        # new state (forward Euler integration)
-        xn     = x     + self.dt*dx
-        yn     = y     + self.dt*dy
-        thetan = theta + self.dt*dtheta
+    # new state (forward Euler integration)
+    xn     = x     + dt*dx
+    yn     = y     + dt*dy
+    thetan = theta + dt*dtheta
 
-        return xn, yn, thetan
+    return xn, yn, thetan
 
-    def evaluate(self, controls, times, verbose=False):
+def car_evaluate(car, controls, times, verbose=False):
 
-        '''
-        Returns a simulated final result, given a sequence of controls
-        and times. controls[i] is considered constant between times[i]
-        and times[i+1].
-        '''
+    '''
+    Returns a simulated final result, given a sequence of controls
+    and times. controls[i] is considered constant between times[i]
+    and times[i+1].
+    '''
 
-        if len(controls) != len(times) - 1:
-            raise ValueError("Control sequence length must be 1 less than times.")
-        elif times[0] != 0:
-            raise ValueError("First time must be 0.")
-        elif any([phi < -pi/4 or phi > pi/4 for phi in controls]):
-            raise ValueError("All controls must be between -pi/4 and pi/4.")
-        elif any([times[i] >= times[i+1] for i in range(len(controls))]):
-            raise ValueError("Time sequence must be increasing.")
-        elif any([times[i+1] - times[i] < self.dt-0.000001 for i in range(len(controls))]):
-            raise ValueError("Difference in each time must be greater than 0.01 [s].")
+    if len(controls) != len(times) - 1:
+        raise ValueError("Control sequence length must be 1 less than times.")
+    elif times[0] != 0:
+        raise ValueError("First time must be 0.")
+    elif any([phi < -pi/4 or phi > pi/4 for phi in controls]):
+        raise ValueError("All controls must be between -pi/4 and pi/4.")
+    elif any([times[i] >= times[i+1] for i in range(len(controls))]):
+        raise ValueError("Time sequence must be increasing.")
 
-        # states and time lists with initial configuration
-        xl     = [self.x0]
-        yl     = [self.y0]
-        thetal = [0]
-        ul     = []
-        tl     = [0]
+    # states and time lists with initial configuration
+    xl     = [car.x0]
+    yl     = [car.y0]
+    thetal = [0]
+    ul     = []
+    tl     = [0]
 
-        for i in range(len(controls)):
+    for i in range(len(controls)):
 
-            # simulate between times
-            while tl[-1] < times[i+1]:
+        # simulate between times
+        while tl[-1] < times[i+1]:
 
-                # get new state and time
-                xn, yn, thetan = self.step(xl[-1], yl[-1], thetal[-1], controls[i])
+            # get new state and time
+            xn, yn, thetan = step(car, xl[-1], yl[-1], thetal[-1], controls[i])
 
-                if verbose:
-                    print("x:", xn, "y:", yn, "theta:", thetan)
+            if verbose:
+                print("x:", xn, "y:", yn, "theta:", thetan)
 
-                # record state
-                xl.append(xn)
-                yl.append(yn)
-                thetal.append(thetan)
+            # record state
+            xl.append(xn)
+            yl.append(yn)
+            thetal.append(thetan)
 
-                # record control
-                ul.append(controls[i])
+            # record control
+            ul.append(controls[i])
 
-                # record time
-                tl.append(tl[-1] + self.dt)
+            # record time
+            tl.append(tl[-1] + 0.01)
 
-                # get safety and doneness
-                safe = self._environment.safe(xn, yn)
-                done = True if ((self.xt-xn)**2 + (self.yt-yn)**2)**0.5 < 1 else False
+            # get safety and doneness
+            safe = car._environment.safe(xn, yn)
+            done = True if ((car.xt-xn)**2 + (car.yt-yn)**2)**0.5 < 1 else False
 
-                # terminate if safe or done
-                if not safe:
-                    term = True
-                    break
-                elif done:
-                    term = True
-                    break
-                else:
-                    term = False
-
-            if term == True:
+            # terminate if safe or done
+            if not safe:
+                term = True
                 break
+            elif done:
+                term = True
+                break
+            else:
+                term = False
 
-        # extend last control to signify constant value between times
-        ul.append(ul[-1])
+        if term == True:
+            break
 
-        # return states, controls, times, final saftey, and done
-        return xl, yl, thetal, ul, tl, safe, done
+    # extend last control to signify constant value between times
+    ul.append(ul[-1])
+
+    # return states, controls, times, final saftey, and done
+    return xl, yl, thetal, ul, tl, safe, done
 
 class Environment(object):
 
