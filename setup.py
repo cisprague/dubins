f63392ef195678f74b1520249eaeb0fcd772984c
Christopher Iliffe Sprague <christopher.iliffe.sprague@gmail.com>
2023-06-08 12:08:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     author='Christopher Iliffe Sprague',
     author_email='christopher.iliffe.sprague@gmail.com',
     package_dir={'dd2410planning' : 'src'},
-    packages=['dd2410planning'],
-    url='https://github.com/cisprague/kth-dd2410-planning',
+    packages=['dubins'],
+    url='https://github.com/cisprague/dubins',
     description='Code for the DD2410 planning assignment at KTH.',
     install_requires=['numpy', 'matplotlib', 'scipy', 'jupyter']
 )
